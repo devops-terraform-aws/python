c12d91967730480a2c417fe993d97f212047a2c7
Developer <ukohae39@gmail.com>
2023-01-29 19:39:47 -0500

    Users can specify AMI to use

--- create_dashboard.py
+++ create_dashboard.py
@@ -5,24 +5,19 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    ami_list = [
+        {"ImageId": "ami-00874d747dde814fa", "Name": "Ubuntu Server 22.04 LTS (HVM)"},
+        {"ImageId": "ami-0778521d914d23bc1", "Name": "Ubuntu Server 20.04 LTS (HVM)"},
+        {"ImageId": "ami-08fdec01f5df9998f", "Name": "Ubuntu Server 18.04 LTS (HVM)"}
+    ]
     if request.method == "POST":
         region = request.form["region"]
         tag_name = request.form["tag_name"]
         instance_type = request.form["instance_type"]
+        selected_ami = request.form["selected_ami"]
         ec2 = boto3.client('ec2', region_name=region)
-        response = ec2.describe_images(
-            Owners=['099720109477'],
-            Filters=[
-                {
-                    'Name':'name',
-                    'Values':["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-                }])
-
-        try:
-            response['Images'].sort(key=lambda x: x['CreationDate'], reverse=True)
-            ami_id = response['Images'][0]['ImageId']
-            response = ec2.run_instances(
-                ImageId=ami_id,
+        response = ec2.run_instances(
+                ImageId=selected_ami,
                 InstanceType=instance_type,
                 MinCount=1,
                 MaxCount=1,
@@ -40,11 +35,9 @@ def index():
                     },
                 ]
             )
-            return render_template("create_index.html", response = "Instance created with ID: {}".format(response['Instances'][0]['InstanceId']))
-        except IndexError:
-            return render_template("create_index.html", response = "No images found that match the specified filters.")
-    else:
-        return render_template("create_index.html")
+        return render_template("create_index.html", amis=ami_list, response = "Instance created with ID: {}".format(response['Instances'][0]['InstanceId']))
+    return render_template("create_index.html", amis=ami_list)
+
 
 if __name__ == "__main__":
     app.run()
