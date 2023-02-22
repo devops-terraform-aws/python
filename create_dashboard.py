84103da067a03456e093bd02743255cecedb60f2
developer <ukohae39@gmail.com>
2023-02-21 20:37:05 -0500

    adding key download option

--- create_dashboard.py
+++ create_dashboard.py
@@ -12,8 +12,15 @@ def index():
         {"ImageId": "ami-08fdec01f5df9998f", "Name": "Ubuntu Server 18.04 LTS (HVM)"},
         {"ImageId": "ami-0dfcb1ef8550277af", "Name": "Amazon Linux 2 AMI (HVM)"}
     ]
+    region = request.form.get("region", "us-east-1") # Set default region as us-east-1
+    ec2 = boto3.client('ec2', region_name=region)
+    
+    # Fetch existing key pairs in the region
+    response = ec2.describe_key_pairs()
+    key_pairs = response.get("KeyPairs", [])
+    key_names = [kp["KeyName"] for kp in key_pairs]
+
     if request.method == "POST":
-        region = request.form["region"]
         tag_name = request.form["tag_name"]
         instance_type = request.form["instance_type"]
         selected_ami = request.form["selected_ami"]
@@ -23,7 +30,9 @@ def index():
         
         # Create the key and download it
         if create_key:
-            ec2 = boto3.client('ec2', region_name=region)
+            new_key_name = request.form.get("new_key_name")
+            if new_key_name:
+                key_name = new_key_name
             key_pair = ec2.create_key_pair(KeyName=key_name)
             with open(key_name + '.pem', 'w') as f:
                 f.write(key_pair['KeyMaterial'])
@@ -31,8 +40,10 @@ def index():
             if download_key:
                 return send_file(io.BytesIO(key_data), as_attachment=True, download_name=key_name+'.pem')
         
+        if key_name not in key_names and not create_key:
+            return render_template("create.html", amis=ami_list, key_names=key_names, error="Invalid key name")
+
         # Create the EC2 instance using the selected AMI, instance type, and key name
-        ec2 = boto3.client('ec2', region_name=region)
         response = ec2.run_instances(
             ImageId=selected_ami,
             InstanceType=instance_type,
@@ -52,8 +63,9 @@ def index():
                 },
             ]
         )
-        return render_template("create_index.html", amis=ami_list, response="Instance created with ID: {}".format(response['Instances'][0]['InstanceId']))
-    return render_template("create_index.html", amis=ami_list)
+        return render_template("create_index.html", amis=ami_list, key_names=key_names, response="Instance created with ID: {}".format(response['Instances'][0]['InstanceId']))
+    return render_template("create_index.html", amis=ami_list, key_names=key_names, region=region)
+
 
 
 if __name__ == "__main__":
